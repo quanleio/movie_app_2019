7c73300b209e1ed85e03408ebd778a487803d6eb
Le Hong Quan <quan.le@letsee.io>
2021-03-22 14:38:00 +0900

    Work with props.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({ name , rating}) {
+  return(
+      <div>
+        <h1>I like { name }</h1>
+        <h4>{ rating }/5.0</h4>
+      </div>
+
+  );
+}
+
+const foodILike = [
+    { id: 1, name: 'potato', image: '', rating: 5},
+    { id: 2, name: 'tomato', image: '', rating: 4.5},
+    { id: 3, name: 'kiwi', image: '', rating: 3.7},
+    { id: 4, name: 'orange', image: '', rating: 6.5}
+    ];
+
+/*function renderFood(item) {
+  return(<Food key={item.id} name={item.name}/>);
+}*/
+
+Food.propType = {
+  name: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      /*<div>{foodILike.map(renderFood)}</div>*/
+      <div>{foodILike.map(item =>
+          <Food
+              key={item.id}
+              name={item.name}
+              rating={item.rating}/>)}
+      </div>
   );
 }
 
