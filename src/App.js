380843a82b86b06d099056a582aaa4b6096e5c2f
Le Hong Quan <quan.le@letsee.io>
2021-03-23 15:31:29 +0900

    make navigation

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
-
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import './App.css';
+import { HashRouter, BrowserRouter, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Navigation from './components/Navigation';
 
-class App extends React.Component {
-
-  state = {
-    isLoading: true,
-    movies: []
-  }
-
-  getMovies = async () => {
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating');
-    this.setState({ movies: movies, isLoading: false });
-  }
-
-  componentDidMount() {
-    this.getMovies();
-  }
-
-  render() {
-    const { isLoading, movies } = this.state;
-    return(
-        <section className='container'>
-          { isLoading ? (
-                  <div className='loader'>
-                    <span className='loader_text'>Loading...</span>
-                  </div>
-              ) : (
-                  <div className='movies'>
-                    {movies.map(movie => {
-                      return (
-                          <Movie
-                              key={movie.id}
-                              id={movie.id}
-                              title={movie.title}
-                              year={movie.year}
-                              summary={movie.summary}
-                              poster={movie.medium_cover_image}
-                              genres={movie.genres}
-                          />)
-                    })}
-                  </div>
-              )}
-        </section>
-    );
-  }
+function App() {
+  return(
+      <HashRouter>
+        <Navigation/>
+        <Route path='/' exact={true} component={Home} />
+        <Route path='/about' component={About}/>
+      </HashRouter>
+  )
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -14,7 +14,7 @@ function Movie({ year, title, summary, poster, genres }) {
               genres.map((genre, index) => <li key={index} className='genres_genre'>{genre}</li>)
             }
           </ul>
-          <p className='movie_summary'> {summary} </p>
+          <p className='movie_summary'> {summary.slice(0, 180)}...</p>
         </div>
       </div>
   )
